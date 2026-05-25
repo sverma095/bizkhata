7816581c2e8427cf02203c1ce1f04264f1287db5
sverma095 <svtiger543939@gmail.com>
2026-05-25 12:34:19 +0530

    Delete lib directory

--- lib/supabase/server.ts
+++
@@ -1,3 +0,0 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-export const createServerClient = () => createServerComponentClient({ cookies });
