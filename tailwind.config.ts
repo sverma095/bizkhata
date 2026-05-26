1278b65a6422c6c568db532f1700d546d210b5a3
sverma095 <svtiger543939@gmail.com>
2026-05-26 17:25:17 +0530

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,3 +0,0 @@
-import type { Config } from "tailwindcss";
-const config: Config = { darkMode: ["class"], content: ["./app/**/*.{ts,tsx}","./components/**/*.{ts,tsx}"], theme: { extend: { borderRadius: { xl: "1rem", "2xl": "1.5rem" } } }, plugins: [require("tailwindcss-animate")] };
-export default config;
